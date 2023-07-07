5b45eecae866e5c4fb3e34b20a48d4f444d5dc8c
brunocitoni <bruno.citoni1@gmail.com>
2023-07-07 21:21:13 +0200

    itemSO script

---
+++ Assets/Scripts/ItemSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDefault", menuName = "ScriptbaleObjects/Item")]
+
+public class ItemSO : ScriptableObject
+{
+    string itemName;
+    Sprite itemSprite;
+}
