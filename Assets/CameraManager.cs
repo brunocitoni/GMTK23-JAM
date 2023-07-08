adfcff037ee05d1c318d94278721f1f360b3c4cd
Borribakbeer <81376276+Borribakbeer@users.noreply.github.com>
2023-07-08 09:56:43 +0200

    Added camera shake

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager instance;
+
+    public CinemachineVirtualCamera virtualCamera;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public static void Shake(float duration, float magnitude)
+    {
+        if(instance != null)
+            instance.StartCoroutine(instance.ShakeIEnumerator(duration, magnitude));
+    }
+
+    IEnumerator ShakeIEnumerator(float duration, float magnitude)
+    {
+        var transposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        transposer.m_TrackedObjectOffset = Vector3.zero;
+
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transposer.m_TrackedObjectOffset = new Vector3(x, y, -10f);
+            elapsed += Time.deltaTime;
+            yield return 0;
+        }
+        transposer.m_TrackedObjectOffset = Vector3.zero;
+    }
+}
