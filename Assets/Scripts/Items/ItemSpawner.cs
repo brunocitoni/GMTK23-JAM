2d6ab345111dee6f18e50bc33d9b2d5c93aade43
brunocitoni <bruno.citoni1@gmail.com>
2023-07-09 07:28:58 +0200

    Update ItemSpawner.cs

--- Assets/Scripts/Items/ItemSpawner.cs
+++ Assets/Scripts/Items/ItemSpawner.cs
@@ -10,7 +10,7 @@ public class ItemSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InstanciateScriptableObject();
+        //InstanciateScriptableObject();
     }
 
     // Update is called once per frame
